0ddd6f56270941471b27e1ef36482005392d4fce
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2022-02-01 13:18:03 -0300

    Add files via upload

--- dados-heroku-esp32/dados-heroku-esp32.ino
+++ dados-heroku-esp32/dados-heroku-esp32.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Adafruit_INA219.h>
+#include <ML8511.h>
 
 const char *ssid = "SATURNO";
 const char *password = "erickkaren";
@@ -9,8 +10,9 @@ const char *serverName = "http://svuv-api.herokuapp.com/data_read/load";
 
 int UVOUT = 36;   /* Pino D36 do ESP32 conetado ao Out do sensor UV*/
 int REF   = 34;   /* Pino D34 do ESP32 conectado ao EN do sensor UV */
-String UV_index = "0";
-String risco = "0";
+
+
+ML8511 light(UVOUT, REF);
 
 WiFiClient client;
 HTTPClient http;
@@ -23,11 +25,18 @@ void printConnectionInformation();
 void setup(void)
 {
    Serial.begin(115200);
-   pinMode(UVOUT, INPUT);
-   pinMode(REF, INPUT);
+  // manually enable / disable the sensor.
+  light.enable();
+
+  // adjust to your ADC specification.
+  light.setVoltsPerStep(3.3, 4095);      // 12 bit DAC
+
+  light.setDUVfactor(1.61);    // calibrate your sensor - default 1.61
+
+  Serial.print("\tmW cm^2");
+  Serial.print("\tDUV index");
+  Serial.println();
   
-  
-  delay(1000);
   while (!Serial) {
     delay(1);
   }
@@ -47,6 +56,7 @@ void setup(void)
 
 void loop(void)
 {
+  String risco = "0";
   float shuntvoltage = 0;
   float busvoltage = 0;
   float loadvoltage = 0;
@@ -56,93 +66,52 @@ void loop(void)
  
   loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-  int uvLevel = averageAnalogRead(UVOUT);           /* Armazena a leitura analógica do pino OUT */
-  int refLevel = averageAnalogRead(REF);            /* Armazena a leitura analógica do pino EN */
-  /* Use 3.3V como referencia no calculo de tensão */
-  float outputVoltage = 3.3 / refLevel * uvLevel; /* Indica a tensão de saída do sensor */
-  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); /* Intensidade raios UV */
+  // 0 - 2  BAIXO
+  // 3 - 5 MODERADO
+  // 6 - 7 ALTO
+  // 8 - 10  MUITO ALTO
+  // 11+ EXTREMO
+  
+  float UV = light.getUV();
+  float DUV = light.estimateDUVindex(UV); // 0 a 15
 
-  int tensao = map(uvLevel, 0, refLevel, 0, 1023);
+  Serial.print(UV, 4);
+  Serial.print("\t");
+  Serial.print(DUV, 1);
+  Serial.println();
   
   //Compara com valores tabela UV_Index
-  if (tensao > 0 && tensao < 50)
+  if (DUV >= 0 && DUV < 3)
   {
-    UV_index = "0";
     risco = "baixo";
   }
-  else if (tensao > 50 && tensao <= 227)
+  else if (DUV >= 3 && DUV < 6)
   {
-    UV_index = "0";
-    risco = "baixo";
-  }
-  else if (tensao > 227 && tensao <= 318)
-  {
-    UV_index = "1";
-    risco = "baixo";
-  }
-  else if (tensao > 318 && tensao <= 408)
-  {
-    UV_index = "2";
-    risco = "baixo";
-  }
-  else if (tensao > 408 && tensao <= 503)
-  {
-    UV_index = "3";
     risco = "moderado";
   }
-  else if (tensao > 503 && tensao <= 606)
+  else if (DUV >= 6 && DUV < 8)
   {
-    UV_index = "4";
-    risco = "moderado";
-  }
-  else if (tensao > 606 && tensao <= 696)
-  {
-    UV_index = "5";
-    risco = "moderado";
-  }
-  else if (tensao > 696 && tensao <= 795)
-  {
-    UV_index = "6";
     risco = "alto";
   }
-  else if (tensao > 795 && tensao <= 881)
+  else if (DUV >= 8 && DUV < 11)
   {
-    UV_index = "7";
-    risco = "alto";
-  }
-  else if (tensao > 881 && tensao <= 976)
-  {
-    UV_index = "8";
     risco = "muito alto";
   }
-  else if (tensao > 976 && tensao <= 1079)
+  else if (DUV >= 11)
   {
-    UV_index = "9";
-    risco = "muito alto";
-  }
-  else if (tensao > 1079 && tensao <= 1170)
-  {
-    UV_index = "10";
-    risco = "muito alto";
-  }
-  else if (tensao > 1170)
-  {
-    UV_index = "11";
     risco = "extremo";
   }
 
-  Serial.print("Classificacao: ");
-  Serial.println (tensao);
-  Serial.println(UV_index);
+  Serial.println("risco: ");
   Serial.println(risco);
-  Serial.println(); 
-  delay(200);
- 
+
+  delay(1000);
+  
   if (WiFi.status() == WL_CONNECTED) {
     http.begin(client, serverName);
 
     http.addHeader("Content-Type", "application/json");
-    String body = "{\"voltage\": "+ String(loadvoltage) + " ,\"uv\": "+ String(UV_index) + ", \"risco\": "+ String(risco) + "}";
+    String body = "{\"voltage\": "+ String(loadvoltage) + " ,\"uv\": "+ String(DUV) + ", \"risco\": "+ String(risco) + "}";
     int httpResponseCode = http.POST(body);
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
@@ -155,26 +124,6 @@ void loop(void)
   }
   
   delay(2000);
-}
-
- 
-//Takes an average of readings on a given pin
-//Returns the average
-int averageAnalogRead(int pinToRead)
-{
-  byte numberOfReadings = 8;
-  unsigned int runningValue = 0; 
- 
-  for(int x = 0 ; x < numberOfReadings ; x++)
-    runningValue += analogRead(pinToRead);
-  runningValue /= numberOfReadings;
- 
-  return(runningValue);
-}
- 
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void connectToInternet() {
